dda0b9c6e69d71f9f2abefa1ad1e0c60d80bcda2
José Antonio <er.joselin@gmail.com>
2024-11-30 10:02:45 +0000

    Últimos cambios desde la máquina original

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '9f0h)gozf$g%6igo8&767w1xro0adm+)msxe)!eic$!fhvynb8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django.jose.com', '172.22.203.221', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
@@ -119,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
