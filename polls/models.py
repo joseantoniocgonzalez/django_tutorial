d455194717f88c57b99cb1c706dd57ee44ada66b
joseantoniocgonzalez <er.joselin@gmail.com>
2025-02-01 08:54:45 +0000

    Configuración de Apache y corrección de archivos estáticos

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,6 @@
 import datetime
-
 from django.db import models
 from django.utils import timezone
-
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -17,7 +15,6 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
